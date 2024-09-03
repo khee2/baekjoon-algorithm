4da5ca65757d282e821234f9681ed721c69329dd
Keunghee Ro <124848492+khee2@users.noreply.github.com>
2024-09-03 12:44:39 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 36 ms, Memory: 31252 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,33 +1,17 @@
 MOD = 1000000000
 
-# 1~100까지 미리 계산
-# 사용하는 인덱스: 0~9, 1~100
-dp = [[0]*11 for _ in range(101)]
+N = int(input())
+
+dp = [[0]*10 for _ in range(N)]
 
 # n=1 초기값 설정
-dp[1][0] = 0 # 0으로 시작하는 수는 계단수가 아님
-dp[1][1] = 1  
-dp[1][2] = 1  
-dp[1][3] = 1  
-dp[1][4] = 1  
-dp[1][5] = 1  
-dp[1][6] = 1  
-dp[1][7] = 1  
-dp[1][8] = 1  
-dp[1][9] = 1  
+dp[0] = [0,1,1,1,1,1,1,1,1,1] # 0으로 시작하는 수는 계단수가 아님
 
+# n=2~N까지 점화식 
+for n in range(1, N):
+    dp[n][0]=dp[n-1][1] # 끝자리가 0
+    dp[n][9]=dp[n-1][8] # 끝자리가 9
+    for m in range(1, 9): # 끝자리가 1~8
+        dp[n][m]= dp[n-1][m-1] + dp[n-1][m+1]
 
-# n=4부터 점화식 
-for n in range(2, 101):
-    for m in range(0, 10):
-        if (m==0):
-            dp[n][0]=dp[n-1][1] 
-        else:
-            dp[n][m]= dp[n-1][m-1] + dp[n-1][m+1]
-
-# 테스트 케이스 입력 처리
-t = int(input())
-total = 0 
-for i in range(0, 10):
-    total += dp[t][i]  # sum에서 total로 변경
-print(total % MOD)
+print(sum(dp[N-1]) % MOD)
