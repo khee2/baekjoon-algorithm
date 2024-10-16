693cc6583de9e7d00851487c050fdb3487429e45
Keunghee Ro <124848492+khee2@users.noreply.github.com>
2024-10-16 23:59:30 +0900

    [Silver I] Title: 스티커, Time: 736 ms, Memory: 50064 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,26 @@
+T = int(input())
+
+for _ in range(T):
+    n = int(input())
+    stickers = [list(map(int, input().split())) for _ in range(2)]
+    
+    if n == 1:
+        print(max(stickers[0][0], stickers[1][0]))
+        continue
+    
+    # DP 테이블 초기화
+    dp = [[0] * n for _ in range(2)]
+    
+    # 첫 번째 열 초기화
+    dp[0][0] = stickers[0][0]
+    dp[1][0] = stickers[1][0]
+    
+    # 두 번째 열 초기화
+    dp[0][1] = stickers[1][0] + stickers[0][1]
+    dp[1][1] = stickers[0][0] + stickers[1][1]
+    
+    for i in range(2, n):
+        dp[0][i] = max(dp[1][i-1], dp[1][i-2]) + stickers[0][i]
+        dp[1][i] = max(dp[0][i-1], dp[0][i-2]) + stickers[1][i]
+    
+    print(max(dp[0][n-1], dp[1][n-1]))
