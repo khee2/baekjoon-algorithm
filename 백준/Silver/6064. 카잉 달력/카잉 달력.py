aff23eb3243ce1dd6d8a4edddf2a509ae378f5ad
Keunghee Ro <124848492+khee2@users.noreply.github.com>
2024-10-23 15:59:15 +0900

    [Silver I] Title: 카잉 달력, Time: 2532 ms, Memory: 33240 KB -BaekjoonHub

--- 백준/Silver/6064. 카잉 달력/카잉 달력.py
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -1,12 +1,18 @@
+import math
+
 T = int(input())
 for _ in range(T):
     M, N, x, y = map(int, input().split())
     
-    i = x  # k를 x로 초기화
-    while i <= M * N:  # k의 범위는 M*N을 넘을 수 없기에
-        if (i - x) % M == 0 and (i - y) % N == 0:
+    # M과 N의 최소공배수 구하기
+    lcm = M * N // math.gcd(M, N)
+    
+    i = x  # i를 x로 초기화
+    while i <= lcm:  # i의 범위는 최소공배수를 넘을 수 없다.
+        # i가 x와 y 모두의 조건을 만족하는지 확인
+        if (i - x) % M == 0 and (i - y) % N == 0: 
             print(i)
             break
-        i += M  # x를 기준으로 M씩 증가시켜 확인
+        i += M  # M씩 증가
     else:
         print(-1)
