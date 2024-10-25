224044de9828d6447bd494589c14b9a269c60932
Keunghee Ro <124848492+khee2@users.noreply.github.com>
2024-10-25 23:53:22 +0900

    [Silver III] Title: N과 M (2), Time: 32 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.py
@@ -0,0 +1,29 @@
+def backtrack(n, m, sequence, visited, start):
+    # sequence안에 m개의 요소가 쌓인다면 출력
+    if len(sequence) == m:
+        print(' '.join(map(str, sequence)))
+        return
+    
+    # 1부터 n까지의 숫자를 하나씩 확인    
+    for i in range(start, n+1):
+        if not visited[i]:
+            # i가 아직 방문되지 않았다면
+            visited[i] = True
+            sequence.append(i)
+            
+            # 다음 숫자를 선택하기 위해 재귀 호출
+            backtrack(n,m,sequence, visited, i+1)
+            
+            # 선택을 되돌리기 (백트래킹)
+            sequence.pop()
+            visited[i] = False
+
+n, m = map(int, input().split())
+
+# 방문 여부를 확인하기 위한 리스트
+visited = [False] * (n + 1)
+
+# 결과를 담을 스택
+sequence = []
+start = 1
+backtrack(n, m, sequence, visited, start)
