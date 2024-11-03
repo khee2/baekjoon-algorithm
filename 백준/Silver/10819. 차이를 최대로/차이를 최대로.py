626aa9c4a7d6bbb4b9e7450fa787b7b8b4272bb7
Keunghee Ro <124848492+khee2@users.noreply.github.com>
2024-11-03 19:06:36 +0900

    [Silver II] Title: 차이를 최대로, Time: 136 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.py
@@ -4,12 +4,12 @@ a.sort()
 
 s = []
 visited = [False] * (n+1)
-
-final = []
+max_sum = 0
 
 def dfs():
+    global max_sum
     if len(s)==n:
-        final.append(s[:])  # 리스트의 복사본을 추가
+        max_sum = max(max_sum, sum(abs(s[j] - s[j + 1]) for j in range(n - 1)))
         return
     for i in range(n):
         if not visited[i]:
@@ -19,17 +19,5 @@ def dfs():
             s.pop()
             visited[i] = False
 dfs()
-
-max_sum = 0
-
-# # final의 모든 요소 출력
-# for perm in final:
-#     print(*perm)
-    
-for i in range(len(final)):
-    cur_sum = 0
-    for j in range(n-1):
-        cur_sum += abs(final[i][j] - final[i][j+1])
-    max_sum = max(max_sum, cur_sum)
 
 print(max_sum)
