15cc6b1eb22a9700913bd5a463769cde80f0349e
Keunghee Ro <124848492+khee2@users.noreply.github.com>
2024-11-06 23:59:54 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,16 +1,15 @@
-t = int(input())
+import sys
 
-dp = {1: 1, 2: 2, 3: 4}  # 처음 값들을 딕셔너리로 초기화
+n = int(input())
 
-for _ in range(t):
-    n = int(input())
-    
-    if n not in dp:
-        for j in range(4, n+1):
-            if j not in dp:  # 아직 dp[j]가 계산되지 않은 경우
-                dp[j] = 0  # dp[j]를 0으로 초기화
-                for k in range(1, 4):  # 1, 2, 3의 합으로 dp[j] 값 계산
-                    dp[j] += dp[j - k]
-    
-    # dp[n] 출력
-    print(dp[n])
+arr = [0] * 11
+arr[1] = 1
+arr[2] = 2
+arr[3] = 4
+
+for i in range(4,11):
+    arr[i] = arr[i-1] + arr[i-2] + arr[i-3]
+
+for i in range(0,n):
+    testNum = int(input())
+    print(arr[testNum])
