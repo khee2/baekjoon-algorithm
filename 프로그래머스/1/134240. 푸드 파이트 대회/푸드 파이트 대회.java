87e2b58faf2838fc15bd30b1fad81b08f3de4c95
Keunghee Ro <124848492+khee2@users.noreply.github.com>
2024-11-13 18:40:00 +0900

    Update 푸드 파이트 대회.java

--- 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
+++ 프로그래머스/1/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -3,9 +3,7 @@ class Solution {
         String answer = "";
         for (int i = 1; i < food.length; i++){
             int count = food[i] / 2;
-            for (int j = 0; j < count; j++){
-                answer += i;
-            }
+            answer += String.valueOf(i).repeat(count);
         }
         StringBuffer r = new StringBuffer(answer);
         return answer + "0" + r.reverse().toString();
