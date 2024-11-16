b671c3c77f62ae11f382f2d799d0c2d590e83449
Keunghee Ro <124848492+khee2@users.noreply.github.com>
2024-11-16 12:24:08 +0900

    [level 1] Title: 모의고사, Time: 0.96 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/README.md
+++ 프로그래머스/1/42840. 모의고사/README.md
@@ -1,10 +1,10 @@
 # [level 1] 모의고사 - 42840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42840) 
 
 ### 성능 요약
 
-메모리: 81.5 MB, 시간: 3.73 ms
+메모리: 73.8 MB, 시간: 0.96 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 11월 16일 12:12:33
+2024년 11월 16일 12:24:06
 
 ### 문제 설명
 
--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,24 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] answer = new int[3];
+        int[] score = new int[3];
         
         int[] pattern1 = {1,2,3,4,5};
         int[] pattern2 = {2,1,2,3,2,4,2,5};
         int[] pattern3 = {3,3,1,1,2,2,4,4,5,5};
 
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == pattern1[i % pattern1.length]) answer[0]++;
-            if (answers[i] == pattern2[i % pattern2.length]) answer[1]++;
-            if (answers[i] == pattern3[i % pattern3.length]) answer[2]++;
+            if (answers[i] == pattern1[i % pattern1.length]) score[0]++;
+            if (answers[i] == pattern2[i % pattern2.length]) score[1]++;
+            if (answers[i] == pattern3[i % pattern3.length]) score[2]++;
         }
         
-        int max = Math.max(answer[0], Math.max(answer[1], answer[2]));
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
         
-        ArrayList<Integer> winners = new ArrayList<>();
-        for (int i = 0 ; i < 3 ; i++){
-            if (answer[i] == max) winners.add(i+1);
+        // 최댓값을 가진 사람의 수 카운트
+        int count = 0;
+        for (int i = 0; i < 3; i++) {
+            if (score[i] == max) count++;
         }
-        return winners.stream().mapToInt(i -> i).toArray();
+        
+        // 결과 배열에 최댓값을 가진 사람 번호 저장
+        int[] answer = new int[count];
+        int index = 0;
+        for (int i = 0; i < 3; i++) {
+            if (score[i] == max) {
+                answer[index++] = i + 1;
+            }
+        }
+        return answer;
     }
 }
