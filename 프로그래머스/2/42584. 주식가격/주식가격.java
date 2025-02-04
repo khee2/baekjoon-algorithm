3f13de83891a1c4144ffa4847f729e10fb91ace7
Keunghee Ro <bluet@sookmyung.ac.kr>
2025-02-04 19:56:54 +0900

    [level 2] Title: 주식가격, Time: 32.19 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,25 +1,25 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] prices) {
-        Stack<Integer> beginIdxs = new Stack<>(); // 주가가 아직 떨어지지 않은 인덱스를 저장하는 스택
-        int i=0;
-        int[] terms = new int[prices.length]; // 결과 배열: prices[i]가 떨어질 때까지 걸린 시간 저장
+        Deque<Integer> beginIdxs = new ArrayDeque<>(); // 주가가 아직 떨어지지 않은 인덱스를 저장
+        int i = 0;
+        int[] terms = new int[prices.length]; // prices[i]가 떨어질 때까지 걸린 시간 저장
 
-        beginIdxs.push(i);
-        
         // 모든 가격을 순회하며 가격이 떨어지는 순간을 찾음
-        for (i=1; i<prices.length; i++) {
-            while (!beginIdxs.empty() && prices[i] < prices[beginIdxs.peek()]) { 
+        beginIdxs.push(i);
+        for (i = 1; i < prices.length; i++) {
+            while (!beginIdxs.isEmpty() && prices[i] < prices[beginIdxs.peek()]) {
                 int beginIdx = beginIdxs.pop();
                 terms[beginIdx] = i - beginIdx;
             }
             beginIdxs.push(i);
         }
-        // 마지막까지 가격이 떨어지지 않은 값 처리
-        while (!beginIdxs.empty()) {
+
+		// 마지막까지 가격이 떨어지지 않은 값 처리
+        while (!beginIdxs.isEmpty()) {
             int beginIdx = beginIdxs.pop();
-            terms[beginIdx] = i - beginIdx - 1; 
+            terms[beginIdx] = i - beginIdx - 1;
         }
 
         return terms;
