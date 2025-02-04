a8b1d5891c8e638045c5e3e303e65f1cc3b93fb4
Keunghee Ro <bluet@sookmyung.ac.kr>
2025-02-04 19:46:06 +0900

    [level 2] Title: 주식가격, Time: 33.83 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,16 +1,27 @@
+import java.util.Stack;
+
 class Solution {
     public int[] solution(int[] prices) {
-		int len = prices.length;
-        int[] answer = new int[len];
+        Stack<Integer> beginIdxs = new Stack<>(); // 주가가 아직 떨어지지 않은 인덱스를 저장하는 스택
+        int i=0;
+        int[] terms = new int[prices.length]; // 결과 배열: prices[i]가 떨어질 때까지 걸린 시간 저장
+
+        beginIdxs.push(i);
         
-        for (int i = 0; i < len; i++) {
-            for (int j = i+1; j < len; j++) {
-		        answer[i]++;
-                if (prices[i] > prices[j]) { // 가격이 떨어진다면 중단
-                    break;
-                }
+        // 모든 가격을 순회하며 가격이 떨어지는 순간을 찾음
+        for (i=1; i<prices.length; i++) {
+            while (!beginIdxs.empty() && prices[i] < prices[beginIdxs.peek()]) { 
+                int beginIdx = beginIdxs.pop();
+                terms[beginIdx] = i - beginIdx;
             }
+            beginIdxs.push(i);
         }
-        return answer;
+        // 마지막까지 가격이 떨어지지 않은 값 처리
+        while (!beginIdxs.empty()) {
+            int beginIdx = beginIdxs.pop();
+            terms[beginIdx] = i - beginIdx - 1; 
+        }
+
+        return terms;
     }
 }
