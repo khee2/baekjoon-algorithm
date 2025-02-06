2b7a55bf2a59ed12de8812c86d5bef46592ff55d
Keunghee Ro <bluet@sookmyung.ac.kr>
2025-02-07 00:41:09 +0900

    [level 2] Title: H-Index, Time: 1.23 ms, Memory: 87.2 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,20 +1,16 @@
-import java.util.Arrays;
+import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
-        Arrays.sort(citations); // [0, 1, 3, 5, 6]
-        int n = citations.length;
-        int hIndex = 0;
-        for (int i = 0; i < n; i++) {
-            int count = citations[i];
-            int least = n - i; // h편 이상
-        
-            if (least >  count) { // h번 이상 논문이 인용되었다면 
-                hIndex = Math.max(hIndex, count);
-            } else {
-                hIndex = Math.max(hIndex, least);
+        Arrays.sort(citations);
+        int hIndex = citations.length;
+
+        for (int citation : citations) {
+            if (citation >= hIndex) {
+                return hIndex;
             }
+            hIndex--;
         }
-        return hIndex;
+        return 0;
     }
 }
